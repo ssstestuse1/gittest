dc656bfd27d4d6d614877207079a15e74ccc0729
ssstestuse1 <102273132+ssstestuse1@users.noreply.github.com>
2022-03-24 20:28:15 +0800

    updategitee

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("push test");
          System.out.println("pull test");
+        System.out.println("gitee-github test");
 
 
 
